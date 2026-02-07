34e49402bb4f9e36c103bc49384b4e56c3ef9ddd
Mohsen20n <162800669+Mohsen20n@users.noreply.github.com>
2026-02-07 14:40:40 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+import requests
+import re
+
+app = Flask(__name__)
+
+# لیست کانال‌های پابلیک (بدون @)
+CHANNELS = [
+    "vpnsal",
+    "VPNConectd",
+    "Tweety_Proxy"
+]
+
+@app.route("/")
+def home():
+    all_configs = []
+
+    for channel in CHANNELS:
+        try:
+            url = f"https://t.me/s/{channel}"
+            html = requests.get(url, timeout=10).text
+
+            configs = re.findall(
+                r'(vmess://\S+|vless://\S+|trojan://\S+|tg://proxy\S+)',
+                html
+            )
+
+            all_configs.extend(configs)
+
+        except:
+            continue
+
+    # حذف تکراری‌ها
+    all_configs = list(set(all_configs))
+
+    if not all_configs:
+        return "چیزی پیدا نشد"
+
+    # نمایش ساده در سایت
+    return "<br>".join(all_configs)
